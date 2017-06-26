50d407d88a94db99fb92adec1b7305de0ea7ef16
User Haru708 <haru708@www903.sakura.ne.jp>
2017-06-26 22:45:16 +0900

    first commit

---
+++ fuel/app/logs/2017/06/26.php
@@ -0,0 +1,4 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+Warning - 2017-06-26 13:32:36 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+Warning - 2017-06-26 13:34:15 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
